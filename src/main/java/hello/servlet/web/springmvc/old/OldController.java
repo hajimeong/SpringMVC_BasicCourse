aa1b42bc4622dc1991ff67a53606940e2d59fdb8
hajimeong <ha_01@naver.com>
2025-09-11 17:12:48 +0900

    뷰리졸버

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -11,6 +11,6 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form"); //논리 이름만 -> 뷰를 못찾음 -> 뷰 리졸버를 만들어줘야 함
     }
 }
